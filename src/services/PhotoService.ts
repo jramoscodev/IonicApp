76e427617e797195f1eca4ca2e5d43fdb406bd0b
jramoscodev <jramos@claroscurodev.com>
2020-06-01 17:14:00 -0600

    fix path

--- src/services/PhotoService.ts
+++ src/services/PhotoService.ts
@@ -3,6 +3,7 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 import { ImagePicker, ImagePickerOptions } from '@ionic-native/image-picker';
 import { FilePath } from '@ionic-native/file-path';
 import { Chooser, ChooserResult } from '@ionic-native/chooser';
+import { Platform } from 'ionic-angular';
 
 
 
@@ -11,7 +12,8 @@ export class PhotoService {
     constructor(private camera: Camera, 
                 private imagePicker: ImagePicker, 
                 private filePath: FilePath,
-                private chooser :Chooser
+                private chooser :Chooser,
+                private platform:Platform
                ) { }
 
     async takePicture() {
@@ -89,10 +91,13 @@ export class PhotoService {
         let fileName = result.name;
         let checkExt = fileName.split('.')[1];
         if(checkExt.toLowerCase() !== 'pdf') return null;
-
-        let pathPdf = await  this.filePath.resolveNativePath(result.uri);
+        let pathPdf : string;
+        if(this.platform.is('android'))
+          pathPdf= await  this.filePath.resolveNativePath(result.uri);
+        else
+          pathPdf = result.uri;
         let response = { path: pathPdf , fileName: fileName }
-
+        console.log('file dir', response);
         return response;
        }catch(e){
            return { error: e };
@@ -109,10 +114,11 @@ export class PhotoService {
             if (result == undefined) return null;
 
             let fileName = result.name;
-           // let checkExt = fileName.split('.')[1];
-           // if (checkExt.toLowerCase() !== 'pdf') return null;
-
-            let pathPdf = await this.filePath.resolveNativePath(result.uri);
+           let pathPdf:string; 
+           if(this.platform.is('android'))
+              pathPdf = await this.filePath.resolveNativePath(result.uri);
+            else
+              pathPdf = result.uri
             let response = { path: pathPdf, fileName: fileName }
             console.log(response)
             return response;
