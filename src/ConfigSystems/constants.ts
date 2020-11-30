39776351b0b06f329acf9ee5041edff7bd29aa9b
jramoscodev <jramos@claroscurodev.com>
2020-11-30 15:44:40 -0600

    fix constat file

--- src/ConfigSystems/constants.ts
+++ src/ConfigSystems/constants.ts
@@ -7,15 +7,15 @@ export class Configuration {
   // public uriServer="http://201.220.133.207:81/";
   // public uriServer="http://201.220.133.218:8007/";
  // public uriServer="https://172.16.1.116:444/";
-    public uriServer = "https://casos.trabajo.gob.hn:444";
+   
  // public uriServer = "https://ecms-backend.conveyor.cloud/";
  // public BaseUrl = "https://ecms-backend.conveyor.cloud/api";
-    public BaseUrl = "https://casos.trabajo.gob.hn:444/api/";
+    
 
   
   //PRODUCION
- // public uriServer = "https://181.210.8.69:443/"
-  //public BaseUrl = "https://181.210.8.69:443/api";
+  public uriServer = "https://casos.trabajo.gob.hn:444";
+  public BaseUrl = "https://casos.trabajo.gob.hn:444/api/";
   public ServerLocal_Security = this.uriServer+'api/seguridad/' ;
   public serverLocalFlujo=this.uriServer+ "api/flujos/";
   public serverCambio=this.uriServer+"api/"
